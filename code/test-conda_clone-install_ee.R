8b9b3ee0c9e5ed3c7875a4403d43a1beb363b102
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2022-11-11 19:40:11 -0500

    added: run-main.sh code/

---
+++ code/test-conda_clone-install_ee.R
@@ -0,0 +1,63 @@
+
+test.conda_clone.install_ee <- function(
+    clone.path   = "cloneCondaEnv",
+    to.be.cloned = "r-reticulate"
+    ) {
+
+    thisFunctionName <- "test.conda_clone.install_ee";
+    cat("\n### ~~~~~~~~~~~~~~~~~~~~ ###");
+    cat(paste0("\n",thisFunctionName,"() starts.\n\n"));
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    require(reticulate);
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    cat("\nclone.path\n");
+    print( clone.path   );
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    if ( dir.exists(clone.path) ) {
+        cat("\nThe conda environment '",clone.path,"' already exists; activating this conda environment ...\n");
+        reticulate::use_condaenv(condaenv = clone.path, required = TRUE);
+        cat("\nThe conda environment '",clone.path,"' has been activated ...\n");
+        DF.conda.list  <- reticulate::conda_list();
+        selected.row   <- base::grepl(x = DF.conda.list[,'python'], pattern = clone.path);
+        my.python.path <- DF.conda.list[selected.row,'python'];
+    } else {
+        cat("\nThe conda environment '",clone.path,"' does not yet exist; cloning the environment '",to.be.cloned,"' ...\n");
+        cat("\nConda environment cloning begins: '",to.be.cloned,"' --> '",clone.path,"'\n");
+        my.python.path <- reticulate::conda_clone(
+            envname = clone.path,
+            clone   = to.be.cloned
+            );
+        cat("\nConda environment cloning complete: '",to.be.cloned,"' --> '",clone.path,"'\n");
+        cat("\nInstallation of earthengine-api begins: '",clone.path,"'\n");
+        reticulate::conda_install(
+            envname  = clone.path,
+            packages = c("earthengine-api"),
+            forge    = TRUE,
+            conda    = "auto"
+            );
+        cat("\nInstallation of earthengine-api complete: '",clone.path,"'\n");
+        }
+
+    cat("\nmy.python.path\n");
+    print( my.python.path   );
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    cat("\nreticulate::py_list_packages(envname = clone.path, type = 'conda')\n");
+    print(
+        reticulate::py_list_packages(
+            envname = clone.path,
+            type    = 'conda'
+            )
+        );
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    cat(paste0("\n",thisFunctionName,"() quits."));
+    cat("\n### ~~~~~~~~~~~~~~~~~~~~ ###\n");
+    return( my.python.path );
+
+    }
+
+##################################################
---
+++ code/test-conda_create.R
@@ -0,0 +1,55 @@
+
+test.conda_create <- function(
+    env.path = "condaEnvGEE"
+    ) {
+
+    thisFunctionName <- "test.conda_create";
+    cat("\n### ~~~~~~~~~~~~~~~~~~~~ ###");
+    cat(paste0("\n",thisFunctionName,"() starts.\n\n"));
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    require(reticulate);
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    cat("\nenv.path\n");
+    print( env.path   );
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    if ( dir.exists(env.path) ) {
+        cat("\nThe conda environment '",env.path,"' already exists; activating this conda environment ...\n");
+        reticulate::use_condaenv(condaenv = env.path, required = TRUE);
+        cat("\nThe conda environment '",env.path,"' has been activated ...\n");
+        DF.conda.list  <- reticulate::conda_list();
+        selected.row   <- grepl(x = DF.conda.list[,'python'], pattern = env.path);
+        my.python.path <- DF.conda.list[selected.row,'python'];
+    } else {
+        cat("\nConda environment creation begins: '",env.path,"'\n");
+        my.python.path <- reticulate::conda_create(
+            envname  = env.path,
+            packages = c("earthengine-api"),
+            forge    = TRUE,
+            conda    = "auto"
+            );
+        cat("\nConda environment creation complete: '",env.path,"'\n");
+        }
+
+    cat("\nmy.python.path\n");
+    print( my.python.path   );
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    cat("\nreticulate::py_list_packages(envname = env.path, type = 'conda')\n");
+    print(
+        reticulate::py_list_packages(
+            envname = env.path,
+            type    = "conda"
+            )
+        );
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    cat(paste0("\n",thisFunctionName,"() quits."));
+    cat("\n### ~~~~~~~~~~~~~~~~~~~~ ###\n");
+    return( my.python.path );
+
+    }
+
+##################################################
